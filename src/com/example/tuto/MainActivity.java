ca4fd8004884fd0bf83cf60ea7df421a1d3fec05
Rouzz <rvionny@gmail.com>
2013-10-15 22:16:42 +0200

    initialisation du projet

---
+++ src/com/example/tuto/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.tuto;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private TextView coucou = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        coucou = new TextView(this);
+        coucou.setText("Hello World");
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
